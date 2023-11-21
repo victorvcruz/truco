8759bf5a635f1dc436e33a04ee176341713001c3
Victor Cruz <victor.cruz@vocedm.com.br>
2023-11-21 17:37:27 -0300

    feat: add game structure

--- jogo.c
+++ jogo.c
@@ -7,51 +7,176 @@
 #define TOTAL_CARTAS 39
 
 int main() {
-    PilhaCarta *topo = NULL;
-    topo = (PilhaCarta *) malloc(sizeof(PilhaCarta));
-    if (topo == NULL) {
-        return 1;
+    srand(time(NULL));
+
+    PilhaCarta *listaCartas = lerArq("cartas.txt");
+    Jogo *jogo = inicializa_estruturas(listaCartas);
+    int modo;
+    int aux = 1;
+    while (aux == 1) {
+        printf("Escolha o modo de jogo:\n");
+        printf("1 - Manual\n");
+        printf("2 - Automático\n");
+        printf("3 - Encerrar\n");
+        scanf("%d", &modo);
+        switch (modo) {
+            case 1:
+                partidas_manual(jogo);
+            case 2:
+                //partidas_automatico(jogo);
+            case 3:
+                printf("Jogo Encerrado!");
+                aux = 0;
+                break;
+        }
     }
-
-    iniciarBaralho(topo);
-
-    PilhaCarta *aux = topo->prox;
-    while (aux->prox != NULL) {
-        printf("%d de %s\n", aux->valor->numero, aux->valor->naipe);
-        aux = aux->prox;
-    }
-
-    printf("\nEMBARALHADAS:\n");
-    embaralhar(topo);
-
-    aux = topo->prox;
-    while (aux->prox != NULL) {
-        printf("%d de %s\n", aux->valor->numero, aux->valor->naipe);
-        aux = aux->prox;
-    }
-    printf("%d de %s\n", aux->valor->numero, aux->valor->naipe);
 }
 
-void push(PilhaCarta * topo, Carta *valor) {
-    PilhaCarta * atual = topo;
+Jogo *inicializa_estruturas(PilhaCarta *listCartas) {
+    Jogador *jogadorA1 = (Jogador *) malloc(sizeof(Jogador));
+    strcpy(jogadorA1->nome, "A1");
+    jogadorA1->cartas = NULL;
 
-    while(atual->prox != NULL) {
+
+    Jogador *jogadorB1 = (Jogador *) malloc(sizeof(Jogador));
+    strcpy(jogadorB1->nome, "B1");
+    jogadorB1->cartas = NULL;
+
+
+    Jogador *jogadorA2 = (Jogador *) malloc(sizeof(Jogador));
+    strcpy(jogadorA2->nome, "A2");
+    jogadorA2->cartas = NULL;
+
+
+    Jogador *jogadorB2 = (Jogador *) malloc(sizeof(Jogador));
+    strcpy(jogadorB2->nome, "B2");
+    jogadorB2->cartas = NULL;
+
+    jogadorA1->ant = jogadorB2;
+    jogadorA1->prox = jogadorB1;
+
+    jogadorB1->ant = jogadorA1;
+    jogadorB1->prox = jogadorA2;
+
+    jogadorA2->ant = jogadorB1;
+    jogadorA2->prox = jogadorB2;
+
+    jogadorB2->ant = jogadorA2;
+    jogadorB2->prox = jogadorA1;
+
+    Mesa *mesa = (Mesa *) malloc(sizeof(Mesa));
+    mesa->jogadores = jogadorA1;
+    mesa->rodada = 0;
+
+    Jogo *jogo = (Jogo *) malloc(sizeof(Jogo));
+    jogo->mesa = mesa;
+    jogo->pontosA = 0;
+    jogo->pontosB = 0;
+    jogo->turno = 0;
+    jogo->vencedor = 'N';
+    jogo->lcartas = listCartas;
+    jogo->carta_virada = NULL;
+
+    return jogo;
+}
+
+void partidas_manual(Jogo *jogo) {
+    distribuir_cartas(jogo);
+
+    while (verifica_vencedor(jogo) == 'N') {
+        printf("-----Turno %d-----\n\n", jogo->turno + 1);
+        for (jogo->mesa->rodada = 0; jogo->mesa->rodada < 3; jogo->mesa->rodada++) {
+            printf("---Rodada %d---\n", jogo->mesa->rodada + 1);
+            Jogador *jogador = jogo->jogador_que_distribuiu->prox;
+            for (int j = 0; j < 4; j++) {
+                printf("--Jogador %s--\n", jogador->nome);
+                printf("Cartas:\n");
+
+                PilhaCarta *cartas = jogador->cartas;
+                jogo->carta_virada = escolherCarta(cartas);
+
+                printf("Carta virada: %d de %s\n", jogo->carta_virada->valor, jogo->carta_virada->naipe);
+
+                jogador = jogador->prox;
+            }
+        }
+    }
+
+    printf("Fim de jogo!\n");
+    printf("O vencedor foi o time %c\n!", verifica_vencedor(jogo));
+    printf("\nDeseja jogar novamente?\n");
+}
+
+char verifica_vencedor(Jogo *jogo) {
+    if (jogo->pontosA >= 12) {
+        return 'A';
+    } else if (jogo->pontosB >= 12) {
+        return 'B';
+    } else {
+        return 'N';
+    }
+}
+
+Carta *escolherCarta(PilhaCarta *cartas) {
+    PilhaCarta *aux = cartas->prox;
+    int escolha = 0;
+    while (aux != NULL) {
+        escolha++;
+        printf("%d - %d de %s\n", escolha, aux->valor->valor, aux->valor->naipe);
+        aux = aux->prox;
+    }
+
+    printf("Escolha uma carta: ");
+    scanf("%d", &escolha);
+    Carta *carta = remover_carta(cartas, escolha);
+    return carta;
+}
+
+void distribuir_cartas(Jogo *jogo) {
+    if (jogo->turno == 0) {
+        int sorteioDistribui = rand() % 4;
+        Jogador *atual = jogo->mesa->jogadores;
+        for (int k = 0; k < sorteioDistribui; k++) {
+            atual = atual->prox;
+        }
+        jogo->jogador_que_distribuiu = atual;
+    } else {
+        jogo->jogador_que_distribuiu = jogo->jogador_que_distribuiu->prox;
+    }
+
+    PilhaCarta *baralho = embaralhar(jogo->lcartas);
+
+    Jogador *jogadores = jogo->jogador_que_distribuiu;
+    for (int i = 0; i < 4; i++) {
+        for (int j = 0; j < 3; j++) {
+            adicionar_carta(jogadores->cartas, pop(baralho));
+        }
+        jogadores = jogadores->prox;
+    }
+}
+
+// adicionar na pilha
+void push(PilhaCarta *topo, Carta *valor) {
+    PilhaCarta *atual = topo;
+
+    while (atual->prox != NULL) {
         atual = atual->prox;
     }
-    atual->prox = (PilhaCarta*) malloc(sizeof(PilhaCarta));
+    atual->prox = (PilhaCarta *) malloc(sizeof(PilhaCarta));
     atual->prox->valor = valor;
     atual->prox->prox = NULL;
 }
 
-Carta * pop(PilhaCarta * topo) {
-    struct Carta * valor;
+// remover da pilha
+Carta *pop(PilhaCarta *topo) {
+    Carta *valor;
     if (topo->prox == NULL) {
         valor = topo->valor;
         free(topo);
         return valor;
     }
 
-    PilhaCarta * atual = topo;
+    PilhaCarta *atual = topo;
     while (atual->prox->prox != NULL) {
         atual = atual->prox;
     }
@@ -62,17 +187,61 @@ Carta * pop(PilhaCarta * topo) {
     return valor;
 }
 
-void embaralhar(PilhaCarta *topo) {
-    srand(time(NULL));
+// adicionar na lista
+void adicionar_carta(PilhaCarta *cartas, Carta *valor) {
+    PilhaCarta *atual = cartas;
+    if(atual == NULL) {
+        atual = (PilhaCarta *) malloc(sizeof(PilhaCarta));
+        atual->valor = valor;
+        atual->prox = NULL;
+        return;
+    }
+
+    while (atual->prox != NULL) {
+        atual = atual->prox;
+    }
+    atual->prox = (PilhaCarta *) malloc(sizeof(PilhaCarta));
+    atual->prox->valor = valor;
+    atual->prox->prox = NULL;
+}
+
+// remover da lista
+Carta *remover_carta(PilhaCarta *cartas, int posicao) {
+    Carta *valor;
+    PilhaCarta *atual = cartas;
+    for (int i = 0; i < posicao; i++) {
+        atual = atual->prox;
+    }
+    valor = atual->prox->valor;
+    free(atual->prox);
+    atual->prox = NULL;
+    return valor;
+}
+
+PilhaCarta *embaralhar(PilhaCarta *topo) {
+    PilhaCarta *baralho = (PilhaCarta *) malloc(sizeof(PilhaCarta));;
+    baralho->valor = NULL;
+    baralho->prox = NULL;
+
+    PilhaCarta *cartas = topo;
+
+    while (cartas->prox != NULL) {
+        Carta *novaCarta = (Carta *) malloc(sizeof(Carta));
+        novaCarta->valor = cartas->prox->valor->valor;
+        strcpy(novaCarta->naipe, cartas->prox->valor->naipe);
+        push(baralho, novaCarta);
+        cartas = cartas->prox;
+    }
+
     for (int indexCarta = 0; indexCarta < TOTAL_CARTAS - 1; indexCarta++) {
         int indexCartaTrocada = rand() % TOTAL_CARTAS;
 
-        PilhaCarta *original = topo->prox;
+        PilhaCarta *original = baralho->prox;
         for (int k = 0; k < indexCarta; k++) {
             original = original->prox;
         }
 
-        PilhaCarta *trocada = topo->prox;
+        PilhaCarta *trocada = baralho->prox;
         for (int k = 0; k < indexCartaTrocada; k++) {
             trocada = trocada->prox;
         }
@@ -81,218 +250,29 @@ void embaralhar(PilhaCarta *topo) {
         original->valor = trocada->valor;
         trocada->valor = aux;
     }
+    return baralho;
 }
 
-void iniciarBaralho(PilhaCarta *topo) {
-    Carta * carta;
-    //4
-    carta = (Carta *) malloc(sizeof(Carta));
-    carta->numero = 4;
-    strcpy(carta->naipe, "Ouro");
-    push(topo, carta);
+PilhaCarta *lerArq(char *nome) {
+    PilhaCarta *topo = NULL;
+    topo = (PilhaCarta *) malloc(sizeof(PilhaCarta));
+    if (topo == NULL) {
+        exit(1);
+    }
 
-    carta = (Carta *)malloc(sizeof(Carta));
-    carta->numero = 4;
-    strcpy(carta->naipe, "Espada");
-    push(topo, carta);
+    FILE *file = fopen("cartas.txt", "r");
+    if (file == NULL) {
+        printf("Erro ao abrir o arquivo.\n");
+        exit(1);
+    }
 
-    carta = (Carta *)malloc(sizeof(Carta));
-    carta->numero = 4;
-    strcpy(carta->naipe, "Copas");
-    push(topo, carta);
+    char linha[20];
+    while (fgets(linha, 20, file) != NULL) {
+        Carta *novaCarta = (Carta *) malloc(sizeof(Carta));
+        sscanf(linha, " %c %[^\n]", &novaCarta->valor, novaCarta->naipe);
+        push(topo, novaCarta);
+    }
 
-    carta = (Carta *)malloc(sizeof(Carta));
-    carta->numero = 4;
-    strcpy(carta->naipe, "Paus");
-    push(topo, carta);
-
-    //5
-    carta = (Carta *)malloc(sizeof(Carta));
-    carta->numero = 5;
-    strcpy(carta->naipe, "Ouro");
-    push(topo, carta);
-
-    carta = (Carta *)malloc(sizeof(Carta));
-    carta->numero = 5;
-    strcpy(carta->naipe, "Espada");
-    push(topo, carta);
-
-    carta = (Carta *)malloc(sizeof(Carta));
-    carta->numero = 5;
-    strcpy(carta->naipe, "Copas");
-    push(topo, carta);
-
-    carta = (Carta *)malloc(sizeof(Carta));
-    carta->numero = 5;
-    strcpy(carta->naipe, "Paus");
-    push(topo, carta);
-
-    //6
-    carta = (Carta *)malloc(sizeof(Carta));
-    carta->numero = 6;
-    strcpy(carta->naipe, "Ouro");
-    push(topo, carta);
-
-    carta = (Carta *)malloc(sizeof(Carta));
-    carta->numero = 6;
-    strcpy(carta->naipe, "Espada");
-    push(topo, carta);
-
-    carta = (Carta *)malloc(sizeof(Carta));
-    carta->numero = 6;
-    strcpy(carta->naipe, "Copas");
-    push(topo, carta);
-
-    carta = (Carta *)malloc(sizeof(Carta));
-    carta->numero = 6;
-    strcpy(carta->naipe, "Paus");
-    push(topo, carta);
-
-    //7
-    carta = (Carta *)malloc(sizeof(Carta));
-    carta->numero = 7;
-    strcpy(carta->naipe, "Ouro");
-    push(topo, carta);
-
-    carta = (Carta *)malloc(sizeof(Carta));
-    carta->numero = 7;
-    strcpy(carta->naipe, "Espada");
-    push(topo, carta);
-
-    carta = (Carta *)malloc(sizeof(Carta));
-    carta->numero = 7;
-    strcpy(carta->naipe, "Copas");
-    push(topo, carta);
-
-    carta = (Carta *)malloc(sizeof(Carta));
-    carta->numero = 7;
-    strcpy(carta->naipe, "Paus");
-    push(topo, carta);
-
-
-    //Q
-    carta = (Carta *)malloc(sizeof(Carta));
-    carta->numero = 11;
-    strcpy(carta->naipe, "Ouro");
-    push(topo, carta);
-
-    carta = (Carta *)malloc(sizeof(Carta));
-    carta->numero = 11;
-    strcpy(carta->naipe, "Espada");
-    push(topo, carta);
-
-    carta = (Carta *)malloc(sizeof(Carta));
-    carta->numero = 11;
-    strcpy(carta->naipe, "Copas");
-    push(topo, carta);
-
-    carta = (Carta *)malloc(sizeof(Carta));
-    carta->numero = 11;
-    strcpy(carta->naipe, "Paus");
-    push(topo, carta);
-
-    //J
-    carta = (Carta *)malloc(sizeof(Carta));
-    carta->numero = 12;
-    strcpy(carta->naipe, "Ouro");
-    push(topo, carta);
-
-    carta = (Carta *)malloc(sizeof(Carta));
-    carta->numero = 12;
-    strcpy(carta->naipe, "Espada");
-    push(topo, carta);
-
-    carta = (Carta *)malloc(sizeof(Carta));
-    carta->numero = 12;
-    strcpy(carta->naipe, "Copas");
-    push(topo, carta);
-
-    carta = (Carta *)malloc(sizeof(Carta));
-    carta->numero = 12;
-    strcpy(carta->naipe, "Paus");
-    push(topo, carta);
-
-    //K
-    carta = (Carta *)malloc(sizeof(Carta));
-    carta->numero = 13;
-    strcpy(carta->naipe, "Ouro");
-    push(topo, carta);
-
-    carta = (Carta *)malloc(sizeof(Carta));
-    carta->numero = 13;
-    strcpy(carta->naipe, "Espada");
-    push(topo, carta);
-
-    carta = (Carta *)malloc(sizeof(Carta));
-    carta->numero = 13;
-    strcpy(carta->naipe, "Copas");
-    push(topo, carta);
-
-    carta = (Carta *)malloc(sizeof(Carta));
-    carta->numero = 13;
-    strcpy(carta->naipe, "Paus");
-    push(topo, carta);
-
-    //A
-    carta = (Carta *)malloc(sizeof(Carta));
-    carta->numero = 1;
-    strcpy(carta->naipe, "Ouro");
-    push(topo, carta);
-
-    carta = (Carta *)malloc(sizeof(Carta));
-    carta->numero = 1;
-    strcpy(carta->naipe, "Espada");
-    push(topo, carta);
-
-    carta = (Carta *)malloc(sizeof(Carta));
-    carta->numero = 1;
-    strcpy(carta->naipe, "Copas");
-    push(topo, carta);
-
-    carta = (Carta *)malloc(sizeof(Carta));
-    carta->numero = 1;
-    strcpy(carta->naipe, "Paus");
-    push(topo, carta);
-
-    //2
-    carta = (Carta *)malloc(sizeof(Carta));
-    carta->numero = 2;
-    strcpy(carta->naipe, "Ouro");
-    push(topo, carta);
-
-    carta = (Carta *)malloc(sizeof(Carta));
-    carta->numero = 2;
-    strcpy(carta->naipe, "Espada");
-    push(topo, carta);
-
-    carta = (Carta *)malloc(sizeof(Carta));
-    carta->numero = 2;
-    strcpy(carta->naipe, "Copas");
-    push(topo, carta);
-
-    carta = (Carta *)malloc(sizeof(Carta));
-    carta->numero = 2;
-    strcpy(carta->naipe, "Paus");
-    push(topo, carta);
-
-    //3
-    carta = (Carta *)malloc(sizeof(Carta));
-    carta->numero = 3;
-    strcpy(carta->naipe, "Ouro");
-    push(topo, carta);
-
-    carta = (Carta *)malloc(sizeof(Carta));
-    carta->numero = 3;
-    strcpy(carta->naipe, "Espada");
-    push(topo, carta);
-
-    carta = (Carta *)malloc(sizeof(Carta));
-    carta->numero = 3;
-    strcpy(carta->naipe, "Copas");
-    push(topo, carta);
-
-    carta = (Carta *)malloc(sizeof(Carta));
-    carta->numero = 3;
-    strcpy(carta->naipe, "Paus");
-    push(topo, carta);
+    fclose(file);
+    return topo;
 }
--- jogo.h
+++ jogo.h
@@ -1,22 +1,151 @@
 #ifndef TRUCO_JOGO_H
 #define TRUCO_JOGO_H
 
-typedef struct carta {
-    int numero;
+/*
+fluxo: jogo truco paulista
+
+1 - ler o arquivo cartas.txt e salva na lista de cartas
+2 - inicializar as estrutura do jogo.
+   2.1 zerar os pontos
+   2.2 atribuir nulo nas struturas
+   2.3
+
+3 - faz um cópias das cartas da lista de cartas para embaralhar
+
+4 - se for o primeira partida, a distribuição será escolhida aleatória e, o outro time começa jogando, a carta virada será a próxima carta da distribuição.
+   4.1 - a partir da próxima partida, a distribuição será pelo próximo a direita.
+   4.2 atualizar a lista de manilhas
+
+5 - A primeira jogada da partida, será o jogador da direita do que destribuiu
+   5.1 - As proxima roda da partida, é retomada por quem ganhou a mão.
+   5.2 Se houver empate na primeira rodada, vira-se a maior carta.
+      5.2.1 Se empatar a segunda, o jogo irá para a terceira rodada
+   5.3 Se empatar a segunda ou a terceira, quem fez a primeira, leva a rodada.
+
+   partida ou rodada - é uma jogada do turno
+   turno - é a melhor de 3 partida
+
+
+   Naipes: ouros (♦ ), espadas (♠), copas (♥) e paus (♣)
+
+*/
+typedef struct carta{
     char naipe[15];
-} Carta;
+    char valor;
+}Carta;
 
 typedef struct PilhaCarta {
     Carta *valor;
     struct PilhaCarta *prox;
 } PilhaCarta;
 
+typedef struct jogador{
+    char nome[2];
+    struct PilhaCarta *cartas;
+    struct jogador *ant;
+    struct jogador * prox;
+}Jogador;
+
+typedef struct mesa{
+    struct jogador *jogadores;
+    unsigned char rodada;
+    char resultado;
+}Mesa;
+
+typedef struct jogo{
+    int pontosA;
+    int pontosB;
+    int turno;
+    struct jogador *jogador_que_distribuiu;
+    struct mesa *mesa;
+    struct PilhaCarta *lcartas;
+    Carta *carta_virada;
+    char vencedor; /* A - venceu ; B - venceu ; N - não tem vencedor ainda */
+
+}Jogo;
+
+/*
+  funcao: lerArq
+  descricao: ler o arquivo de cartas
+  entrada: nome do arquivo
+  retorno: uma lista de cartas
+
+*/
+PilhaCarta *lerArq(char *nome);
+
+/*
+  funcao: inicializa_estruturas
+  descricao: inicializa as estruturas do jogo
+  entrada:
+  retorno: a estrutura jogo
+
+*/
+Jogo *inicializa_estruturas(PilhaCarta *listCartas);
+
+
+
+/*
+  funcao: turnos_manual
+  descricao: realiza os turnos de um jogo manual
+  entrada: jogo
+  retorno: resultados: A - Vencedor ; B - Vencedor
+
+*/
+char turnos_manual(Jogo *jogo);
+
+
+/*
+  funcao: turnos_automatico
+  descricao: realiza os turnos de um jogo automático
+  entrada: jogo
+  retorno: resultados: A - Vencedor ; B - Vencedor
+
+*/
+char turnos_automatico(Jogo *jogo);
+
+
+/*
+  funcao: partidas_manual
+  descricao: realiza as partidas_manual de um jogo manual
+  entrada: jogo
+  retorno: resultados:
+
+*/
+void partidas_manual(Jogo *jogo);
+
+
+/*
+  funcao: partidas_automática
+  descricao: realiza as partidas automática de um jogo automático
+  entrada: jogo
+  retorno: resultados:
+
+*/
+void partidas_automatica(Jogo *jogo);
+
+
+
+/*
+  funcao: verifica_vencedor
+  descricao: realiza a contagem de pontos  de um jogo
+  entrada: jogo
+  retorno: resultados: A - Vencedor ; B - Vencedor ; N - sem vencedor
+
+*/
+char verifica_vencedor(Jogo *jogo);
+
 void push(PilhaCarta * topo, Carta *valor);
 
-Carta * pop(PilhaCarta * topo);
+PilhaCarta * embaralhar(PilhaCarta *topo);
 
-void embaralhar(PilhaCarta *topo);
+Carta *pop(PilhaCarta *topo);
 
-void iniciarBaralho(PilhaCarta *topo);
+void distribuir_cartas(Jogo *jogo);
+
+Carta * escolherCarta(PilhaCarta *cartas);
+
+Carta * remover_carta(PilhaCarta *cartas, int posicao);
+
+void adicionar_carta(PilhaCarta *cartas, Carta *valor);
 
 #endif //TRUCO_JOGO_H
